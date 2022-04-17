5668367a1bbce4541c2944eab25519500772d970
artem <cyberstolyarov@gmail.com>
2022-04-17 10:49:43 +0300

    Добавлена форма ответа (есть ошибка)

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from app.views import (
-    ThemeListView,
-    ThemeDetailView,
+    AnswerCheckView,
     ExerciseDetailView,
+    ThemeDetailView,
+    ThemeListView,
 )
 
 urlpatterns = [
-    path('', ThemeListView.as_view(), name='theme-list'),
+    path('', ThemeListView.as_view(), name='theme_list'),
+    path('answer-check/', AnswerCheckView.as_view(), name='answer_check'),
     path('theme-detail/<int:pk>', ThemeDetailView.as_view(), name='theme_detail'),
     path('exercise-detail/<int:pk>', ExerciseDetailView.as_view(), name='exercise_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+from django.views import View
 from django.views.generic import ListView, DetailView
 
 from app.models import (
@@ -23,3 +25,15 @@ class ThemeDetailView(DetailView):
 
 class ExerciseDetailView(DetailView):
     model = Exercise
+
+
+class AnswerCheckView(View):
+    def post(self, request, *args, **kwargs):
+        successful = False
+        pk = kwargs.get('pk', None)
+        answer = request.POST.get('answer', None)
+        exercise = self.model.objects.get(pk=pk)
+        correct_answer = exercise.correct_answer
+        if answer == correct_answer:
+            successful = True
+        return JsonResponse({"successful": successful})
